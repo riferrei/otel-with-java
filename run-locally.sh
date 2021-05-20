1eb3104d3489a549ecb56797f11f1ccabfb01395
riferrei <riferrei@riferrei.com>
2021-05-20 10:12:41 -0400

    Cleaning the demo for usage

--- run-locally.sh
+++ run-locally.sh
@@ -3,15 +3,3 @@
 mvn clean package -Dmaven.test.skip=true
 
 java -jar target/hello-app-1.0.jar
-
-#OTEL_AGENT=opentelemetry-javaagent-all.jar
-#if [ ! -f "${OTEL_AGENT}" ]; then
-#  wget -O opentelemetry-javaagent-all.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.2.0/opentelemetry-javaagent-all.jar
-#fi
-
-#java -javaagent:./${OTEL_AGENT} \
-#-Dotel.traces.exporter=otlp \
-#-Dotel.metrics.exporter=otlp \
-#-Dotel.exporter.otlp.endpoint=http://localhost:8200 \
-#-Dotel.resource.attributes=service.name=hello-app,service.version=1.0 \
-#-jar target/hello-app-1.0.jar
