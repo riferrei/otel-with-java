66f1d9e6f3cb2adb8e0764cafd7760977b647cab
riferrei <riferrei@riferrei.com>
2021-06-17 10:27:14 -0400

    Fixed Java agent download URL

--- run-locally.sh
+++ run-locally.sh
@@ -4,7 +4,7 @@ mvn clean package -Dmaven.test.skip=true
 
 AGENT_FILE=opentelemetry-javaagent-all.jar
 if [ ! -f "${AGENT_FILE}" ]; then
-  curl -L https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.2.0/opentelemetry-javaagent-all.jar --output ${AGENT_FILE}
+  curl -L https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent-all.jar --output ${AGENT_FILE}
 fi
 
 export OTEL_TRACES_EXPORTER=otlp
