0c90427a9758e51c0b56946ba2ca5a376ada115f
riferrei <riferrei@riferrei.com>
2021-10-21 09:42:04 -0400

    Added support for Elastic Cloud

--- run-locally.sh
+++ run-locally.sh
@@ -10,7 +10,14 @@ fi
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=otlp
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:8200
-#export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Bearer apm_secret_token"
+
+#################################### Run with Elastic Cloud ####################################
+
+# export OTEL_EXPORTER_OTLP_ENDPOINT=https://clusterid.apm.region.provider.elastic-cloud.com:443
+# export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Bearer APM_SECRET_TOKEN"
+
+################################################################################################
+
 export OTEL_RESOURCE_ATTRIBUTES=service.name=hello-app,service.version=1.0
 
 java -javaagent:./${AGENT_FILE} -jar target/hello-app-1.0.jar
