b2f65a39c75c292c0a0d3247b56fefa60f572b88
Ricardo Ferreira <riferrei@riferrei.com>
2022-09-17 14:15:43 -0400

    Please say LGTM 😅

---
+++ src/test/java/tutorial/buildon/aws/o11y/MostImportantTest.java
@@ -0,0 +1,12 @@
+package tutorial.buildon.aws.o11y;
+
+import org.junit.Test;
+
+public class MostImportantTest {
+
+    @Test
+    public void testIfEverythingWorks() {
+        // Nothing to say here
+    }
+    
+}
